a31bf08da1908ccce9ca018b7a7e87c2999ac7c8
MayorErnest <eobumma@gmail.com>
2021-05-10 01:59:40 +0100

    complete table page UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { Header } from "./components/header";
+import { Table } from "./pages/table";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Table />
     </div>
   );
 }
